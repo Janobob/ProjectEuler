5dd69da9270a55d3c2879cfd790124b150f7274d
Jan Werren <jan.werren@outlook.com>
2022-10-08 22:03:30 +0200

    feat: problem5

---
+++ Problem5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var divideUntil = 20;
+            var currentProduct = 0;
+            var found = false;
+
+            while(!found)
+            {
+                currentProduct += 2;
+                var positiveRun = true;
+
+                for (var i = 1; i <= divideUntil; i++)
+                {
+                    if(currentProduct % i != 0)
+                    {
+                        positiveRun = false;
+                        break;
+                    }
+                }
+
+                found = positiveRun;
+            }
+
+            Console.WriteLine(currentProduct);
+        }
+    }
+}
