c3787cc437638615c413a9ed06a914791c1ec8c7
Jan Werren <jan.werren@outlook.com>
2022-10-08 21:56:15 +0200

    feat: problem4

--- Problem4/Program.cs
+++ Problem4/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int largestPalindrome = 0;
+
+            for(int multiplicand = 999; multiplicand > 0; multiplicand--)
+            {
+                for(int multiplicator = 999; multiplicator > 0; multiplicator--)
+                {
+                    var product = multiplicand * multiplicator;
+                    var productString = product.ToString();
+                    var firstPart = string.Concat(productString.Take(3));
+                    var secondPart = string.Concat(productString.Skip(3).Take(3).Reverse());
+
+                    if (firstPart == secondPart && largestPalindrome < product)
+                    {
+                        largestPalindrome = product;
+                    }
+                }
+            }
+
+            Console.WriteLine(largestPalindrome);
         }
     }
 }
