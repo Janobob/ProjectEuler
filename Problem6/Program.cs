ea8e2e295bc82aa35baf41ac20385fb99ce8a19a
Jan Werren <jan.werren@outlook.com>
2022-10-08 22:12:38 +0200

    feat: problem6

---
+++ Problem6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1,100);
+
+            var sumOfSquares = numbers.Sum(x => Math.Pow(x, 2));
+            var squareOfSum = Math.Pow(numbers.Sum(), 2);
+
+            var result = squareOfSum - sumOfSquares;
+            Console.WriteLine(result);
+        }
+    }
+}
