4d3e6f558aaa69a8e3435c8f660d919851a379c1
Jan Werren <jan.werren@outlook.com>
2022-10-08 23:23:39 +0200

    feat: problem8

---
+++ Problem10/Program.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammRunner;
+
+namespace Problem10
+{
+    internal class Program : IProgramm<int>
+    {
+        static void Main(string[] args)
+        {
+            ProgrammRunner<int>.Run(new Program());
+        }
+
+        public int Execute()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Problem9/Program.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammRunner;
+
+namespace Problem9
+{
+    internal class Program : IProgramm<int>
+    {
+        static void Main(string[] args)
+        {
+            ProgrammRunner<int>.Run(new Program());
+        }
+
+        public int Execute()
+        {
+            return 0;
+        }
+    }
+}
