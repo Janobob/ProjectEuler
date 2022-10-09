6322cc1feae59e888751446c0ee3fa037570eb66
Jan Werren <jan.werren@outlook.com>
2022-10-09 22:57:35 +0200

    feat: problem9

--- Problem9/Program.cs
+++ Problem9/Program.cs
@@ -11,7 +11,30 @@ namespace Problem9
 
         public int Execute()
         {
-            return 0;
+            var a = 0;
+            var b = 0;
+            var c = 0;
+            var s = 1000;
+            var found = false;
+            for (a = 1; a < s / 3; a++)
+            {
+                for (b = a; b < s / 2; b++)
+                {
+                    c = s - a - b;
+
+                    if (a * a + b * b == c * c)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    break;
+                }
+            }
+            return a * b * c;
         }
     }
 }
