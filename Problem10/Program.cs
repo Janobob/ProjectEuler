883f9d48c1e0080ad76ec0415438ff9900569220
Jan Werren <jan.werren@outlook.com>
2022-10-12 21:29:37 +0200

    feat: problem10

--- Problem10/Program.cs
+++ Problem10/Program.cs
@@ -2,16 +2,42 @@
 
 namespace Problem10
 {
-    internal class Program : IProgramm<int>
+    internal class Program : IProgramm<long>
     {
         static void Main(string[] args)
         {
-            ProgrammRunner<int>.Run(new Program());
+            ProgrammRunner<long>.Run(new Program());
         }
 
-        public int Execute()
+        public long Execute()
         {
-            return 0;
+            long max = 2000000;
+            long sum = 0;
+
+            for(long i = 2; i < max; i++)
+            {
+                if (IsPrime(i))
+                {
+                    sum += i;
+                }
+            }
+            
+            return sum;
+        }
+
+        public bool IsPrime(long number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (long)Math.Floor(Math.Sqrt(number));
+
+            for (long i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
